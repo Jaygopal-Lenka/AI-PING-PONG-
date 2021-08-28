5c520ae73b7d932bde2047655305c3fc47663b4b
Jaygopal-Lenka <73235238+Jaygopal-Lenka@users.noreply.github.com>
2021-08-28 17:18:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var paddle2Y = 685,paddle2Height = 70;
 
 var score1 = 0, score2 =0;
 var paddle1Y;
-
+var mouseY = 0;
 var  playerscore =0;
 var audio1;
 var pcscore =0;
@@ -19,69 +19,109 @@ var ball = {
     dx:3,
     dy:3
 }
+//////////////////////////////////////////////////////////////////////////////////////////////////////// MY CODE STARTS HERE
+rightWristY = 0;
+rightWristX = 0;
+scoreRightWrist = 0;
 
+game_status = "";
+
+ function preload() {
+  ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+  missed = loadSound("missed.wav");
+}
 
 function setup(){
-  var canvas =  createCanvas(700,600);
-  canvas.parent('canvas1');
-  
-  video = createCapture(VIDEO);
-  video.size(700, 600);
-  video.hide();
-  
-  poseNet = ml5.poseNet(video, modelLoaded);
-  }
-  
-  function modelLoaded() {
-    console.log('PoseNet Is Initialized');
-  }
+var canvas =  createCanvas(700,600);
+canvas.parent('canvas1');
 
+video = createCapture(VIDEO);
+video.size(700, 600);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+
+    rightWristY = results[0].pose.rightWrist.y;
+    rightWristX = results[0].pose.rightWrist.x;
+    scoreRightWrist =  results[0].pose.keypoints[10].score;
+    console.log(scoreRightWrist);
+  }
+}
+
+function startGame()
+{
+  game_status = "start";
+  document.getElementById("status").innerHTML = "Game Is Loading";
+}
 
 function draw(){
- background(0);
- 
-image(video, 0 , 0 , 700 , 600);
- 
 
- fill("black");
- stroke("black");
- rect(680,0,20,700);
+  background(0); 
+  image(video, 0, 0, 700, 600);
 
- fill("black");
- stroke("black");
- rect(0,0,20,700);
- 
-   //funtion paddleInCanvas call 
-   paddleInCanvas();
- 
-   //left paddle
-   fill(250,0,0);
+  fill("black");
+  stroke("black");
+  rect(680,0,20,700);
+
+  fill("black");
+  stroke("black");
+  rect(0,0,20,700);
+
+  if(scoreRightWrist > 0.2)
+  {
+    fill("red");
+    stroke("red");
+    circle(rightWristX, rightWristY, 30);
+  }
+
+  if(game_status == "start")
+  {
+    document.getElementById("status").innerHTML = "Game Is Loaded";
+    //funtion paddleInCanvas call 
+    paddleInCanvas();
+        
+    //left paddle
+    fill(250,0,0);
     stroke(0,0,250);
     strokeWeight(0.5);
-   paddle1Y = mouseY; 
-   rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
-   
-   
+    paddle1Y = rightWristY; 
+    rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
+
+
     //pc computer paddle
     fill("#FFA500");
     stroke("#FFA500");
-   var paddle2y =ball.y-paddle2Height/2;  rect(paddle2Y,paddle2y,paddle2,paddle2Height,100);
+    var paddle2y =ball.y-paddle2Height/2;  rect(paddle2Y,paddle2y,paddle2,paddle2Height,100);
     
     //function midline call
     midline();
     
     //funtion drawScore call 
-   drawScore();
-   
-   //function models call  
-   models();
-   
-   //function move call which in very important
+    drawScore();
+
+    //function models call  
+    models();
+
+    //function move call which in very important
     move();
-}
 
+    }
 
+  }
+  //////////////////////////////////////////////////////////////////////////////////////////////////////// MY CODE ENDS HERE
 
+  
 //function reset when ball does notcame in the contact of padde
 function reset(){
    ball.x = width/2+100,
